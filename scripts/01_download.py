f62febcabf609ea93a7c306f5d8cf39e5cce5a16
David Hodel <davidhodel6@gmail.com>
2025-09-23 18:03:01 +0200

    add data download script

---
+++ scripts/01_download.py
@@ -0,0 +1,29 @@
+import argparse
+from pathlib import Path
+import zipfile
+
+import kaggle
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--target_dir", help="Target directory to download data into", required=False, type=Path)
+    args = parser.parse_args()
+
+download_path = args.target_dir if args.target_dir else Path(__file__).parent.parent / Path("data")
+if not download_path.exists():
+    raise ValueError(f"The target download directory {download_path} does not exist")
+
+kaggle.api.authenticate()
+
+print("Starting download of zip file")
+kaggle.api.competition_download_files("hms-harmful-brain-activity-classification", path=download_path, quiet=False, force=False)
+print("Finished download")
+
+print("Unzipping zip archive")
+zip_path = download_path / Path("hms-harmful-brain-activity-classification.zip")
+with zipfile.ZipFile(zip_path, "r") as zip_file:
+    zip_file.extractall(download_path)
+print("Finished unzipping")
+
+print("Deleting zip archive")
+zip_path.unlink()
