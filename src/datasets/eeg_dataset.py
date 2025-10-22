a7ce972ef105275e6554931aabfe16b3cc812928
Maiko Trede <maikotrede@gmail.com>
2025-10-22 13:01:10 +0200

    Inference Fixes

--- src/datasets/eeg_dataset.py
+++ src/datasets/eeg_dataset.py
@@ -27,7 +27,23 @@ class EEGDataset(Dataset):
             eeg_path = f"{self.data_path}/processed/eegs_parquet/{row.eeg_id}.parquet"
         else:
             eeg_path = f"{self.data_path}/test_eegs/{row.eeg_id}.parquet"
-        eeg_data = pd.read_parquet(eeg_path).values.astype(np.float32)
+
+        eeg_df = pd.read_parquet(eeg_path)
+        eeg_data = eeg_df[Constants.EEG_FEATURES].values.astype(np.float32)
+
+        if self.mode == "test":
+            rows = len(eeg_data)
+            offset = (rows - 10_000) // 2
+            eeg_data = eeg_data[offset : offset + 10_000]
+
+            if np.isnan(eeg_data).any():
+                for j in range(eeg_data.shape[1]):
+                    col_data = eeg_data[:, j]
+                    if np.isnan(col_data).mean() < 1.0:
+                        mean_val = np.nanmean(col_data)
+                        eeg_data[:, j] = np.nan_to_num(col_data, nan=mean_val)
+                    else:
+                        eeg_data[:, j] = 0
 
         filtered_eeg = sosfiltfilt(self.sos, eeg_data, axis=0)
 
@@ -38,15 +54,12 @@ class EEGDataset(Dataset):
 
         mean = np.mean(eeg_to_process, axis=0, keepdims=True)
         std = np.std(eeg_to_process, axis=0, keepdims=True)
-        epsilon = 1e-6
-
-        normalized_eeg = (eeg_to_process - mean) / (std + epsilon)
+        normalized_eeg = (eeg_to_process - mean) / (std + 1e-6)
 
         signals = torch.from_numpy(normalized_eeg.copy()).float()
 
         if self.mode == "test":
             return signals
         else:
-            numeric_values = row[Constants.TARGETS].values.astype(np.float32)
-            labels = torch.tensor(numeric_values, dtype=torch.float32)
+            labels = torch.tensor(row[Constants.TARGETS].values.astype(np.float32), dtype=torch.float32)
             return signals, labels
